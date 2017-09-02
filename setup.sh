e7231511f9e785c9072967270eb90ce01ef47597
Junghwan John Goh <jhgoh@cern.ch>
2017-09-02 13:28:24 +0200

    move base release

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 yum -y install glibc-devel
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-scramv1 project CMSSW CMSSW_8_0_26_patch1
-cd CMSSW_8_0_26_patch1/src
+scramv1 project CMSSW CMSSW_8_0_29
+cd CMSSW_8_0_29/src
 eval `scramv1 runtime -sh`
 gcc -v
 ln -s /CATTools .
